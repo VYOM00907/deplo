65e465e0c82131fe9dd3abaed0bbbc8168ba529a
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2023-06-07 18:07:38 +0530

    Update hagoo.py

--- hagoo.py
+++ hagoo.py
@@ -5,12 +5,10 @@ import time
 from sys import exit
 
 
-def lonc():
-    subprocess.run(["bash","-c","nohup python3 testowa/tester.py"])
+
+subprocess.run(["bash","-c","nohup python3 testowa/tester.py"])
 
 
     
-t1 = Process(target=lonc, args=())
-t1.start()
-    
+
     
