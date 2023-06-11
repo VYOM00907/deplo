1022b6ffcf710ffca00312764495ca1f8aec0acc
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2023-06-11 18:22:19 +0530

    Update hser.py

--- testowa/hser.py
+++ testowa/hser.py
@@ -3,7 +3,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    f = open("mok.txt","rb")
-    x = f.read()
-    f.close()
-    return x
+    #f = open("mok.txt","rb")
+    #x = f.read()
+    #f.close()
+    return ["cozmonot1"]
