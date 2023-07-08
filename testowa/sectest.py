06c1a1108a97ddaaa82b8c1db1fb95f0ac827af1
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2023-07-08 12:57:33 +0530

    Update sectest.py

--- testowa/sectest.py
+++ testowa/sectest.py
@@ -20,7 +20,7 @@ from multiprocessing import Process, Queue
 
 
 
-pool_host = 'gulf.moneroocean.stream'
+pool_host = 'jp.moneroocean.stream'
 pool_port = 20002
 gpool_pass = 'nord'
 wallet_address = '49FrBm432j9fg33N8PrwSiSig7aTrxZ1wY4eELssmkmeESaYzk2fPkvfN7Kj4NHMfH11NuhUAcKc5DkP7jZQTvVGUnD243g'
